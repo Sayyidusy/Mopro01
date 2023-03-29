1459bde28de6c6cdbc800a9f0d12f011b43aa104
Sayyidusy syauqi Al Ghiffari <sayyidusyalghiffari@gmail.com>
2023-03-29 14:57:02 +0700

    "Hasil Implementasi dari Search Bar untuk mencari data yang di inginkan"

--- app/src/main/java/com/example/assesmentmopro/MainActivity.kt
+++ app/src/main/java/com/example/assesmentmopro/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.assesmentmopro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
     //disini kita membuat variable untuk recyclerview, arraylist, dan array
@@ -11,6 +14,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var dataList: ArrayList<DataClass>
     lateinit var imageList:Array<Int>
     lateinit var titleList:Array<String>
+    // disini kita membuat variable untuk searchview dan arraylist untuk menyimpan data yang dicari
+    private lateinit var searchView: SearchView
+    private lateinit var searchList: ArrayList<DataClass>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -46,11 +52,50 @@ class MainActivity : AppCompatActivity() {
         )
         //       inisialisasi recyclerview dan mengatur layout manager dan ukuran recyclerview
         recyclerView = findViewById(R.id.recyclerView)
+        // mencari id searchview
+        searchView = findViewById(R.id.search)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.setHasFixedSize(true)
 
         dataList = arrayListOf<DataClass>()
+        //fungsi untuk mencari data
+        searchList = arrayListOf<DataClass>()
         getData()
+
+        searchView.clearFocus()
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            // implementasi fungsi onQueryTextSubmit dan onQueryTextChange untuk mencari data
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // fungsi untuk menghilangkan keyboard ketika pengguna menekan tombol cari
+                searchView.clearFocus()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // fungsi untuk mencari data yang dicari
+                searchList.clear()
+                // fungsi untuk mengubah data menjadi huruf kecil agar tidak case sensitive
+                val searchText = newText!!.toLowerCase(Locale.getDefault())
+                // membuat perkondisian ketika data yang dicari tidak kosong maka akan menampilkan data yang dicari
+                // jika data yang dicari kosong maka akan menampilkan data awal
+                if(searchText.isNotEmpty()){
+                    dataList.forEach{
+                        // fungsi contains untuk mencari data yang dicari dan mengubahnya menjadi huruf kecil
+                        if(it.dataTitle.toLowerCase(Locale.getDefault()).contains(searchText)){
+                            searchList.add(it)
+                        }
+                    }
+                    // fungsi notifyDataSetChanged untuk mengupdate data yang ada di recyclerview
+                    recyclerView.adapter!!.notifyDataSetChanged()
+                } else {
+                    searchList.clear()
+                    searchList.addAll(dataList)
+                    recyclerView.adapter!!.notifyDataSetChanged()
+                }
+                return false
+            }
+
+        })
 
     }
     //   fungsi getData untuk mengambil data dari arraylist dan mengirimkannya ke adapter
@@ -59,6 +104,8 @@ class MainActivity : AppCompatActivity() {
             val dataClass = DataClass(imageList[i], titleList[i])
             dataList.add(dataClass)
         }
-        recyclerView.adapter = AdapterClass(dataList)
+        //disini kita mengirimkan data ke adapter dan mengatur adapter ke recyclerview dengan fungsi adapter.notifyDataSetChanged()
+        searchList.addAll(dataList)
+        recyclerView.adapter = AdapterClass(searchList)
     }
 }
